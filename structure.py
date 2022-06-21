e8bdd27e9d4ae31b8e480dd002c6fdfcd2b130e3
Amanda Wei <amandawei00@gmail.com>
2022-06-21 16:59:29 -0700

    first commit

---
+++ structure.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class Structure:
+    def __init__(self, coords, paxis=[0,0,1], format='cart'):
+        self.coords = coords
+        self.paxis  = paxis
+
+        if format=='cart':
+            for i in coords:
+                
+        elif format == 'sph':
+
+        else
+    def rot(self, axis, theta):
+
+    def ref(self, axis):
+
+    def search_rot(self):
+    def search_ref(self):
+
+    def
